1af5d21a37fc2b3af0d6eeabd7a1e784b6fd16c5
avis709 <avischeung709@gmail.com>
2026-04-17 04:28:22 +0800

    Delete quick_sort.py

--- quick_sort.py
+++
@@ -1,9 +0,0 @@
-def partition(thelist, low, high):
-  pivot = thelist[high]
-  i = low -1
-  for j in range(low, high):
-  if thelist[j] <= pivot:
-    i = i + 1
-    (thelist[i], thelist[j]) = (thelist[j], thelist[i])
-  (thelist[i + 1], thelist[high]) = (thelist[high], thelist[i + 1])
-  return i + 1
