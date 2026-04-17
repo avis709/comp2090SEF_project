abae6476c1a0a33d8891221fe6e49b41a54a5755
avis709 <avischeung709@gmail.com>
2026-04-17 10:43:17 +0800

    Update main.py

--- task1/main.py
+++ task1/main.py
@@ -1,4 +1,5 @@
 from classes import race_rec, race_100meter, race_300meter, race_400mrelay, race_longjump, RaceFactory
+from checking import check()
 
 
 
@@ -97,26 +98,6 @@ def register():
 
 
 
-
-def check():
-    try:
-            user_ID = input("Please insert your ID: ")
-            if int(user_ID) != int(user.id):
-                print("Non-existent UID")
-            else:
-                print("")
-                user.getname()
-                user.getid()
-                user.gethouse()
-                user.getscore()
-                if 3000 <= int(user_ID) < 4000 and isinstance(user, race_400mrelay):
-                    user.getteam()
-                    print("\nTeammates:")
-                    for i, teammate in enumerate(user.teammates, 1):
-                        print(f"   {i}. {teammate.name} - Score: {teammate.score} {teammate.get_unit()}")
-                print("")
-        except ValueError:
-            print("Invalid ID format. Please enter numbers only.")
 
 
 
